b454cdd9f81b50f675a0ccb4773a64d4259c7e5a
Dhiego Emmanuel <dhiegoemmanuelsouza@gmail.com>
2024-07-10 15:54:28 -0300

    Primeiro commit dos estudos

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using system;
+
+Console.WriteLine("Digite o primeiro valor para soma:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("\nDigite o segundo valor para a soma: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"{num1} + {num2} = {num1 + num2}");
